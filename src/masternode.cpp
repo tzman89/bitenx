6627d57ee5f9e48e0a7e27a159a82208c457fe57
tzman89 <35830811+tzman89@users.noreply.github.com>
2018-06-11 14:29:52 +0200

    Update masternode.cpp

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -257,7 +257,7 @@ bool CMasternode::IsInputAssociatedWithPubkey()
     uint256 hash;
     if(GetTransaction(vin.prevout.hash, tx, Params().GetConsensus(), hash, true)) {
         BOOST_FOREACH(CTxOut out, tx.vout)
-            if(out.nValue == 100000*COIN && out.scriptPubKey == payee) return true;
+            if(out.nValue == 5000*COIN && out.scriptPubKey == payee) return true;
     }
 
     return false;
